48f5877728b27c488d1337b2cde6cc566d24be9d
Alejandro Alfaro <alejo9226@gmail.com>
2020-11-11 17:57:26 -0500

    Add dashboard-top

---
+++ src/pages/dashboard/components/UserSection.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+export const UserTopBarDiv = styled.div`
+  display: flex;
+  color: white;
+  align-items: center;
+  box-sizing: border-box;
+`
+
+export const WelcomeMsg = styled.h3`
+  color: white;
+  font-size: 12px;
+`
+
+class UserSection extends React.Component {
+
+  render () {
+    return (
+      <UserTopBarDiv>
+        <WelcomeMsg>¡Bienvenido, {this.props.name}!</WelcomeMsg>
+        <img src=""/>
+        <i></i>  
+      </UserTopBarDiv>
+    )
+  }
+}
+
+export default UserSection
