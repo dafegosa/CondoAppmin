db2d237f57b1169d8c8f528ad03c421b2c12d061
Alejandro Alfaro <alejo9226@gmail.com>
2020-11-10 20:50:47 -0500

    Create dashboard page and component
    Create dashboard page and its component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect
+} from 'react-router-dom';
 import React from 'react'
-function App() {
-  return (
-   <h2>
-     CondoAppmin
-   </h2>
-  );
+import styled from 'styled-components';
+import Dashboard from './pages/Dashboard';
+
+
+class App extends React.Component {
+  render () {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/dashboard" component={Dashboard} />
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App;
