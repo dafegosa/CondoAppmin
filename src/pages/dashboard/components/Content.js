46f5e12a40a00ed0d55a6175dfaeda5a786dd696
Alejandro Alfaro <alejo9226@gmail.com>
2020-11-12 12:18:25 -0500

    Add Dashboard top (#3)
    * Add dashboard-top
    
    * Add dashboard-top
    
    * Add dashboard-top fixed

---
+++ src/pages/dashboard/components/Content.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ContentDiv = styled.div`
+  background-color: rgba(0, 0, 0, 0.05);
+  box-sizing: border-box;
+  display: flex;
+  overflow: hidden;
+  width: 70vw;
+  height: 100vh;
+
+`
+class Content extends React.Component {
+  render () {
+    return (
+      <ContentDiv>
+
+      </ContentDiv>
+    )
+  }
+}
+
+export default Content
---
+++ src/pages/dashboard/components/TopBar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from 'styled-components'
+import UserSection from './UserSection'
+
+
+export const TopBarDiv = styled.div`
+  background-color: ${props => props.theme.mainColor};
+  display: flex;
+  justify-content: flex-end;
+  position: absolute;
+  right: 0;
+  box-sizing: border-box;
+  overflow: hidden;
+  width: 85vw;
+  height: 10vh;
+`
+
+
+class TopBar extends React.Component {
+
+  render () {
+    return (
+     <TopBarDiv>
+       <UserSection name={this.props.name}/>
+     </TopBarDiv>
+    )
+  }
+}
+
+export default TopBar
