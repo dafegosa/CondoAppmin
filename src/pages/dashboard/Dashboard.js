ff1573a038cbce6f221d5d441196a2dcd920d7e3
dafegosa <danielgomez.s@hotmail.com>
2020-11-12 23:26:22 -0500

    Create data folder, and messajes_data file

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import styled from 'styled-components'
-import TopBar from './components/TopBar'
-import Content from './components/Content'
-
+import React from "react";
+import styled from "styled-components";
+import TopBar from "./components/TopBar";
+import Content from "./components/Content";
+import MessagesArea from "./components/MessagesArea";
 
 export const DashboardDiv = styled.div`
   box-sizing: border-box;
@@ -10,20 +10,18 @@ export const DashboardDiv = styled.div`
   overflow: hidden;
   width: 100vw;
   height: 100vh;
-
-`
-
+`;
 
 class Dashboard extends React.Component {
-
-  render () {
+  render() {
     return (
       <DashboardDiv>
-        <TopBar name={'Alejandro'}/>
+        <TopBar name={"Alejandro"} />
+        <MessagesArea />
         <Content />
       </DashboardDiv>
-    )
+    );
   }
 }
 
-export default Dashboard
+export default Dashboard;
