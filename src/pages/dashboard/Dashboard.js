793bb19cecc08747e4be25e3d2eef0cc30c1d3d9
Alejandro Alfaro <alejo9226@gmail.com>
2020-11-14 16:13:02 -0500

    Add dashboard top user section styles and responsiveness

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -4,12 +4,18 @@ import TopBar from './components/TopBar'
 import Content from './components/Content'
 
 
-export const DashboardDiv = styled.div`
+const DashboardDiv = styled.div`
   box-sizing: border-box;
-  display: flex;
+  display: grid;
+  grid-template: repeat(8, 1fr) / repeat(12, 1fr);
+  grid-gap: 0px;
   overflow: hidden;
   width: 100vw;
   height: 100vh;
+
+  @media (max-width: 500px) {
+    grid-template: repeat(12, 1fr) / repeat(8, 1fr);
+  }
 
 `
 
