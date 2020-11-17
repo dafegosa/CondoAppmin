8ab0726a537c7abf68aa69ac4ba5a142e05498b8
Prometheus <alejandrolopez1756@gmail.com>
2020-11-17 18:51:02 -0500

    Fixed dashboard-left list structure

--- src/pages/dashboard/components/LeftMenu.js
+++ src/pages/dashboard/components/LeftMenu.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import logo from '../../../logo.svg'
 import { Link } from 'react-router-dom'
