3c17fab932cab27df3f8c8a41117ce71f9780b96
Prometheus <alejandrolopez1756@gmail.com>
2020-11-16 19:20:49 -0500

    Add left menu

--- src/pages/dashboard/components/LeftMenu.js
+++ src/pages/dashboard/components/LeftMenu.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import logo from '../../../logo.svg'
+import { Link } from 'react-router-dom'
 
 const Container = styled.div`
   background-color: #ffbf5b;
@@ -13,13 +14,13 @@ const Logo = styled.div`
 `
 
 const SideMenu = styled.div`
-  text-align: center;
-  padding-top: 30px;
+  padding-top: 20px;
+  margin-left: 20px;
 
   & li {
     padding: 16px;
     cursor: pointer;
-    transition: 0.15s ease-in;
+    transition: 0.2s ease-in;
 
     &:hover {
       background-color: #607d8b;
@@ -28,6 +29,18 @@ const SideMenu = styled.div`
 
     &:active {
       font-size: 1.1rem;
+    }
+
+    & i {
+      color: #607d8b;
+      margin-right: 10px;
+    }
+
+    & span {
+      &:hover {
+        color: white;
+        text-decoration: none;
+      }
     }
   }
 `
@@ -45,13 +58,38 @@ const LeftMenu = () => {
       </Logo>
 
       <SideMenu>
-        <ol>
-          <li>Mensaje</li>
-          <li>Pagos</li>
-          <li>Eventos</li>
-          <li>Tickets</li>
-          <li>Crear perfil</li>
-        </ol>
+        <ul>
+          <li>
+            <Link>
+              <i className='fas fa-envelope'></i>
+              <span>Mensajes</span>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <i className='fas fa-money-check-alt'></i>
+              <span>Pagos</span>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <i className='fas fa-calendar-alt'></i>
+              <span>Eventos</span>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <i className='fas fa-comment-dots'></i>
+              <span>Tickets</span>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <i className='fas fa-user-plus'></i>
+              <span>Crear perfil</span>
+            </Link>
+          </li>
+        </ul>
       </SideMenu>
 
       <Picture></Picture>
