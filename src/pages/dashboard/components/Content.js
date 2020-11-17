695f19989ef780b875f267cab333eecdb881c242
Alejandro Alfaro <alejo9226@gmail.com>
2020-11-16 19:30:44 -0500

    Dashboard top user (#6)
    * Add dashboard top user section styles and responsiveness
    
    * Add dashboard top user styles and responsiveness and requested changes

--- src/pages/dashboard/components/Content.js
+++ src/pages/dashboard/components/Content.js
@@ -3,18 +3,25 @@ import styled from 'styled-components'
 
 const ContentDiv = styled.div`
   background-color: rgba(0, 0, 0, 0.05);
+  grid-area: 2 / 3 / 9 / 11;
+  padding: 10px;
   box-sizing: border-box;
   display: flex;
   overflow: hidden;
-  width: 70vw;
-  height: 100vh;
 
+  @media (max-width: 768px) {
+    grid-area: 2 / 2 / 9 / 13;
+  }
+
+  @media (max-width: 500px) {
+    grid-area: 2 / 1 / 13 / 9;
+  }
 `
 class Content extends React.Component {
   render () {
     return (
       <ContentDiv>
-
+        <h1>Contenido de la página</h1>
       </ContentDiv>
     )
   }
--- src/pages/dashboard/components/TopBar.js
+++ src/pages/dashboard/components/TopBar.js
@@ -5,14 +5,19 @@ import UserSection from './UserSection'
 
 export const TopBarDiv = styled.div`
   background-color: ${props => props.theme.mainColor};
+  grid-area: 1 / 3 / 2 / 13;
   display: flex;
   justify-content: flex-end;
-  position: absolute;
   right: 0;
   box-sizing: border-box;
-  overflow: hidden;
-  width: 85vw;
-  height: 10vh;
+
+  @media (max-width: 768px) {
+    grid-area: 1 / 2 / 2 / 13;
+  }
+  
+  @media (max-width: 500px) {
+    grid-area: 1 / 1 / 2 / 9;
+  }
 `
 
 
