7b4559d394c8d88007131776f57a54d92f6d9d81
Alejandro Alfaro <alejo9226@gmail.com>
2020-11-23 08:43:34 -0500

    Home (#10)
    * Add Homepage with login and signup links
    
    * Delete unneeded file
    
    * Remove unneeded file from App.js
    
    * Add styles to Login form
    
    * Add home, login, and register routes
    
    * Add home, login, and register routes with changes requested

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import Dashboard from './pages/dashboard/Dashboard';
-import './App.css';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect,
+} from 'react-router-dom'
+import React from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import Home from './pages/home/Home'
+import Dashboard from './pages/dashboard/Dashboard'
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
 
 const theme = {
   mainColor: 'rgba(96, 125, 139, 1)',
@@ -13,12 +19,14 @@ const theme = {
 }
 
 class App extends React.Component {
+
   render() {
     return (
       <header className="App-header">
         <Router>
           <Switch>
             <ThemeProvider theme={theme}>
+              <Route exact path="/" component={Home} />
               <Route exact path="/dashboard" component={Dashboard} />
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
