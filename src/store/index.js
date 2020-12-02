288f17e9b377d0e97c0e2fb424b5de05fbd140cd
Alejandro Alfaro <alejo9226@gmail.com>
2020-12-02 13:15:44 -0500

    remove unused functions on store

--- src/store/index.js
+++ src/store/index.js
@@ -7,67 +7,6 @@ import unitReducer from './unitReducer'
 import residentReducer from './residentReducer'
 import messageReducer from './messageReducer'
 
-export function getUser(history) {
-  return async function (dispatch) {
-
-    const token = localStorage.getItem('token')
-
-    try { 
-      var getAdmin = await axios({
-        method: 'GET',
-        baseURL: 'http://localhost:8000',
-        url: '/admin',
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-    } catch (err) {}
-
-    try {
-      var getResident = await axios({
-        method: 'GET',
-        baseURL: 'http://localhost:8000',
-        url: '/resident',
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-    } catch (err) {}
-
-      if (getAdmin) {
-        dispatch({ type: 'loggedAdmin' })
-        return {getAdmin, type: 'admin'}
-        
-      } else if (getResident) {
-        dispatch({ type: 'loggedResident' })
-        return {getResident, type: 'resident'}
-    
-      } else {
-        localStorage.removeItem('token')
-        history.push('/login')
-      }
-  }
-}
-export function retrieveMessages(user, type) {
-  return async function (dispatch) {
-
-    const token = localStorage.getItem('token')
-
-    try {
-      const { data } = await axios({
-        method: 'GET',
-        baseURL: 'http://localhost:8000',
-        url: `/${type}/${user}?read=false`,
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      dispatch({ type: 'retrieveMessages', payload: data.data }) 
-      return data.data
-    } catch (err) {}
-  }
-}
-
 const rootReducer = combineReducers({ sessionReducer, condoReducer, unitReducer, residentReducer, messageReducer })
 const middlewares = applyMiddleware(thunk)
 
